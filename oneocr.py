7dcb706eda98d185636f4bf23ddb9cb4d3d2c4d6
AuroraWright <AuroraWright@users.noreply.github.com>
2025-10-14 15:15:51 +0200

    Don't resize as it breaks bboxes, only report wrong image sizes in the response

--- oneocr.py
+++ oneocr.py
@@ -1,6 +1,7 @@
 import ctypes
 import sys
 import os
+import copy
 from ctypes import Structure, byref, POINTER, c_int64, c_int32, c_float, c_ubyte, c_char, c_char_p
 from PIL import Image
 from contextlib import contextmanager
@@ -166,12 +167,10 @@ class OcrEngine:
 
     def recognize_pil(self, image):
         '''Process PIL Image object'''
-
-        if any(x < 51 for x in image.size):
-            resize_factor = max(51 / image.width, 51 / image.height)
-            new_w = int(image.width * resize_factor)
-            new_h = int(image.height * resize_factor)
-            image = image.resize((new_w, new_h), Image.Resampling.LANCZOS)
+        if any(x < 50 or x > 10000 for x in image.size):
+            result = copy.deepcopy(self.empty_result)
+            result['error'] = 'Unsupported image size'
+            return result
 
         if image.mode != 'RGBA':
             image = image.convert('RGBA')
@@ -192,17 +191,10 @@ class OcrEngine:
         import cv2
 
         img = cv2.imdecode(image_buffer, cv2.IMREAD_UNCHANGED)
-
-        h, w = img.shape[:2]
-        if w < 51 or h < 51:
-            resize_factor = max(51 / w, 51 / h)
-            new_w = int(w * resize_factor)
-            new_h = int(h * resize_factor)
-            img = cv2.resize(
-                img, 
-                (new_w, new_h), 
-                interpolation=cv2.INTER_LANCZOS4
-            )
+        if any(x < 50 or x > 10000 for x in img.shape[:2]):
+            result = copy.deepcopy(self.empty_result)
+            result['error'] = 'Unsupported image size'
+            return result
 
         # Convert to BGRA format expected by DLL
         channels = img.shape[2] if len(img.shape) == 3 else 1
